0846a7b83711273a439b13c5f4f7c15d2f8e16cc
OleguerCanal <oleguer.canal@xyzrobotics.ai>
2019-07-01 21:50:47 +0200

    tested in GPU

--- scripts/callbacks.py
+++ scripts/callbacks.py
@@ -11,11 +11,11 @@ class TelegramSummary(Callback):
     def on_epoch_end(self, epoch, logs={}):
         print("Sending results to telegram")
         loss = "loss: " + str(round(logs.get('loss'), 2))
-        acc = "acc: " + str(round(logs.get('acc'), 2))
+        acc = "acc:  " + str(round(logs.get('acc'), 2))
         val_loss = "val loss: " + str(round(logs.get('val_loss'), 2))
-        val_acc = "val acc: " + str(round(logs.get('val_acc'), 2))
+        val_acc = "val acc:  " + str(round(logs.get('val_acc'), 2))
 
-        summary = loss + "\n" + acc + "\n" + val_loss + "\n" + val_acc
+        summary = loss + "  " + val_loss + "\n" + acc + "  " + val_acc
         
         telegram_bot = TelegramBot()  #TODO(oleguer): Initialize it before
         answer = telegram_bot.send(summary)
