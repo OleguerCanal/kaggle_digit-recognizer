130e2ed32435ae4e87f4c3a1fda734d9ec9629a6
oleguer <oleguer.canal@hotmail.com>
2019-07-02 14:30:32 +0200

    Merge-ready

---
+++ scripts/evaluate.py
@@ -0,0 +1,12 @@
+from keras.models import model_from_json
+import numpy
+import os
+
+if __name__ == "__main__":
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
